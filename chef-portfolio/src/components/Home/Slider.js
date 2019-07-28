b3f7e9d55cae20239507d8f8ff7b9aad54cec7e9
ShaunKolich <employmeinc@gmail.com>
2019-07-28 19:22:58 -0400

    Finished Slider

--- chef-portfolio/src/components/Home/Slider.js
+++ chef-portfolio/src/components/Home/Slider.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getFood } from '../../Action/Action';
 import styled from 'styled-components';
+var Coverflow = require('react-coverflow');
 
 
 class Slider extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.setState = {
+            // foods: this.props.foods,
+            newfood: this.props.foods[0]
+        }
+
+
+
+
+    }
 
     componentDidMount() {
         this.props.getFood()
     }
 
-
-    render() {
-
+        render() {
+        console.log(this.props.newfood)
         const SliderWrapper = styled.div`
         
         li{
@@ -22,47 +34,73 @@ class Slider extends React.Component {
             justify-c2ontent: center;
             justify-content: space-between;
             margin: 20px 30px;
-            // border: 1px solid yellow;
+            //  border: 1px solid red;
+             width:150px;
+             font-size:12px;
+             line-height:-1;
+             color:white;
                
         }
         img{
-            height:50px;
-            width:50px;
+            height:50%;
+            width:80%;
         }
 
         `;
 
-            const SliderInfo = styled.div`
+        const SliderInfo = styled.div`
             display: flex;
-            width: 80%;
-            height: 200px;
+            width: 100%;
+            background-color: #2d3436;
+            background-image: linear-gradient(315deg, #2d3436 0%, #000000 74%);
+            height: 500px;
             justify-content: center;
             align-items: center;
             overflow-x:hidden; 
-            border:2px solid yellow; 
+            // border:2px solid yellow;
+            font-size:12px;
+             line-height:-1; 
 
         `;
 
+
         return (
-            <SliderWrapper>
-                <SliderInfo>
-                {this.props.foods.map((food) => {
-                    return (
-                        <div key={food.id}>
-                            <li>{food.chef_name}</li>
-                            <li>{food.recipe_title}</li>
-                            <img src = {food.item_photo}/>
-                            <li>{food.chef_location}</li>
-                        </div>
+            <div>
+
+                <SliderWrapper>
+                    <SliderInfo>
+                        <Coverflow width={1060}
+                            height={480}
+                            displayQuantityOfSide={2}
+                            navigation={false}
+                            enableHeading={false}
+                        >
+                            {this.props.foods.map((food) => {
+                                return (
+                                    <div key={food.id}>
+                                        <img src={food.item_photo}/>
+                                        <li>{food.chef_name}</li>
+                                        <li>{food.recipe_title}</li>
+                                        <li>{food.chef_location}</li>
 
 
-                    )
+
+                                    </div>
+
+                                )
 
 
 
-                })}
-                    </SliderInfo>
-            </SliderWrapper>
+                            })}
+
+                        </Coverflow>,
+                </SliderInfo>
+                </SliderWrapper>
+
+
+
+            </div>
+
         )
     }
 }
