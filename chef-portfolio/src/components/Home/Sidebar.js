8e1af7f8e8582b584f7a3688b3c7b49b33fa164c
ShaunKolich <employmeinc@gmail.com>
2019-08-01 20:48:36 -0400

    Added Hamburger

--- chef-portfolio/src/components/Home/Sidebar.js
+++ chef-portfolio/src/components/Home/Sidebar.js
@@ -1,44 +1,21 @@
 
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import React from 'react';
+import MenuLinks from '../Home/MenuLinks'
 
 
-class Toggle extends Component {
+function SideBar() {
+    return (
+      // <div id="App">
+      <div id="page-wrap">
+      <MenuLinks pageWrapId={"page-wrap"} outerContainerId={"App"} />
 
-    state = {
-        on: false,
-    }
+      {/* <div id="page-wrap"> */}
+        
+      </div>
+    // </div>
+    );
+  }
 
-    toggle = () => {
-        this.setState({
-            on: !this.state.on
-        })
-    }
-
-    render() {
-
-        return (
-            <div>
-                <div className="hamburgerIcon">
-                    <i className="fas fa-bars" onClick={this.toggle}></i>
-                </div>
-                {this.state.on && <div className='list'>
-                    <ul>
-                        <li><Link to={'/'} className="nav-link">Home</Link></li>
-                        <li><Link to={'/services'} className="nav-link">Services</Link></li>
-                        <li><a href="#" className="nav-link">About Us</a></li>
-                        <li><Link to={'contactUs'} className="nav-link">Contact Us</Link></li>
-                    </ul>
-                </div>}
-
-
-            </div>
-
-        )
-    }
-
-}
-
-
-export default Toggle;
+export default SideBar;
