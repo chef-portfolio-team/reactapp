8476228e152ccb95440e4b92b1063f61f7db7979
Sir-Williams <sirjohnwilliams@live.com>
2019-08-02 18:41:38 -0700

    finished styling

--- chef-portfolio/src/components/Home.js
+++ chef-portfolio/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from '../components/Home/Slider'
 import Recipes from './Home/Recipes'
 import RecipeForm from './RecipeForm'
-import { Link } from 'react-router-dom'
+import { Route, } from 'react-router-dom'
 import './Home/Home.css'
 
 function Home (){
