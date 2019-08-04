431dc637e8a1d9edbef7d8f2ad88c6890617fd4e
ShaunKolich <employmeinc@gmail.com>
2019-08-04 19:22:26 -0400

    Fixing the menubar

--- chef-portfolio/src/components/Home/MenuLinks.js
+++ chef-portfolio/src/components/Home/MenuLinks.js
@@ -2,20 +2,40 @@ import React from "react";
 import { slide as Menu } from "react-burger-menu";
 import '../../css/Styles.css'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default props => {
+  const StyledList = styled.ul`
+  list-style:none;
+  color:white;
+  text-decoration:none;
+  outline : none;
+  height:50px!important;
+  li{
+    color:white!important;
+    outline : none;
+  }
+  .nav-link{
+    color:white!important;
+    text-decoration:none;
+  }
+
+  `;
+  
   return (
+  
+  
     // Pass on our props
     <Menu {...props}>
 
-      <ul>
+      <StyledList>
         <li><Link to={'/'} className="nav-link">Home</Link></li>
         <li><Link to={'/Login'} className="nav-link">Login</Link></li>
         <li><Link to={'/Register'} className="nav-link">Register</Link></li>
         <li><Link to={'/chef-contact'} className="nav-link">Contact</Link></li>
         <li><Link to={'/team'} className="nav-link">Team</Link></li>
         <li><Link to={'/about'} className="nav-link">About</Link></li>
-      </ul>
+      </StyledList>
 
     </Menu>
   );
