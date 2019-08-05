7aec9c3c3345eaec717d8af644ee107663f4c48b
ShaunKolich <employmeinc@gmail.com>
2019-08-04 20:05:20 -0400

    Fixed white space

--- chef-portfolio/src/components/Login.js
+++ chef-portfolio/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../Action/Action';
+import styled from 'styled-components';
 
 
 class Login extends Component {
@@ -20,15 +21,24 @@ class Login extends Component {
 
     render() {
         console.log(this.state, "from Login")
+
+        const BackGround = styled.div`
+        background-color: #2d3436;
+        background-image: linear-gradient(315deg, #2d3436 0%, #000000 74%);
+        width:100%;
+
+        `;
+
         return (
-            <div>
+            <BackGround>
                 <h1>Please Login:</h1>
                 <form onSubmit={this.login}>
                     <input type="text" placeholder="Username" value={this.state.username} name="username" onChange={this.changeHandler}></input>
                     <input type="text" placeholder="Password" value={this.state.password} name="password" onChange={this.changeHandler}></input>
                     <button>Login</button>
                 </form>
-            </div>
+            </BackGround>
+            
         );
     }
 }
